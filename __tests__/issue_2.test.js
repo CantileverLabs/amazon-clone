feadb91f48b882c92ebaa989e6463248b983d8a3
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2024-01-10 13:48:21 +0530

    test scripts added

---
+++ __tests__/issue_2.test.js
@@ -0,0 +1,25 @@
+describe("Issue #2", () => {
+  beforeAll(async () => {
+    this.page = await globalThis.__BROWSER_GLOBAL__.newPage();
+    await this.page.goto("http://localhost:3000/login");
+    await this.page.setViewport({ width: 1280, height: 1024 });
+  });
+
+  it("Password field should be disabled if email is invalid", async () => {
+    const inputField1Selector =
+      ".login__container > form > input[type='email']";
+    const inputField2Selector =
+      ".login__container > form > input[type='password']";
+
+    // Input text into the first input field
+    const textToInput = "Hello, Puppeteer!";
+    await this.page.type(inputField1Selector, textToInput);
+
+    // Check if the adjacent input field is read-only
+    const isInputField2ReadOnly = await this.page.$eval(
+      inputField2Selector,
+      (inputField) => inputField.readOnly
+    );
+    expect(isInputField2ReadOnly).toBe(true);
+  });
+});
---
+++ __tests__/issue_3.test.js
@@ -0,0 +1,22 @@
+describe("Issue #3", () => {
+  beforeAll(async () => {
+    this.page = await globalThis.__BROWSER_GLOBAL__.newPage();
+    await this.page.goto("http://localhost:3000");
+    await this.page.setViewport({ width: 1280, height: 1024 });
+  });
+
+  it("should not be able to order with negative qty", async () => {
+    const buttonSelector = ".home__row > div:nth-child(1) > button";
+    const anchorTagSelector = ".header__optionBasket";
+    await this.page.click(buttonSelector);
+    await this.page.click(anchorTagSelector);
+
+    const minusButtonSelector =
+      ".checkoutProduct_btn_group > button:nth-child(1)";
+    await this.page.click(minusButtonSelector);
+
+    const productElement = await this.page.$(".checkoutProduct");
+
+    expect(productElement).toBeFalsy();
+  });
+});
