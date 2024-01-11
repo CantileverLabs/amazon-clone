786db60fe244d214601b7cd7f83bbed3b068b5fa
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2024-01-11 11:12:49 +0530

    actions testing

--- __tests__/issue_2.test.js
+++ __tests__/issue_2.test.js
@@ -3,27 +3,27 @@ describe("Issue #2", () => {
     this.page = await globalThis.__BROWSER_GLOBAL__.newPage();
     await this.page.goto("http://localhost:3000/login");
     await this.page.setViewport({ width: 1280, height: 1024 });
-    console.log(await this.page.title())
+    console.log(await this.page.title());
     await this.page.screenshot({
-      path: "./testing.img"
+      path: "./testing.img",
     });
   });
 
   it("Password field should be disabled if email is invalid", async () => {
-    // const inputField1Selector =
-    //   ".login__container > form > input[type='email']";
-    // const inputField2Selector =
-    //   ".login__container > form > input[type='password']";
+    const inputField1Selector =
+      ".login__container > form > input[type='email']";
+    const inputField2Selector =
+      ".login__container > form > input[type='password']";
 
-    // // Input text into the first input field
-    // const textToInput = "Hello, Puppeteer!";
-    // await this.page.type(inputField1Selector, textToInput);
+    // Input text into the first input field
+    const textToInput = "Hello, Puppeteer!";
+    await this.page.type(inputField1Selector, textToInput);
 
-    // // Check if the adjacent input field is read-only
-    // const isInputField2ReadOnly = await this.page.$eval(
-    //   inputField2Selector,
-    //   (inputField) => inputField.readOnly
-    // );
-    // expect(isInputField2ReadOnly).toBe(true);
+    // Check if the adjacent input field is read-only
+    const isInputField2ReadOnly = await this.page.$eval(
+      inputField2Selector,
+      (inputField) => inputField.readOnly
+    );
+    expect(isInputField2ReadOnly).toBe(true);
   });
 });
--- __tests__/issue_3.test.js
+++ __tests__/issue_3.test.js
@@ -6,17 +6,17 @@ describe("Issue #3", () => {
   });
 
   it("should not be able to order with negative qty", async () => {
-    // const buttonSelector = ".home__row > div:nth-child(1) > button";
-    // const anchorTagSelector = ".header__optionBasket";
-    // await this.page.click(buttonSelector);
-    // await this.page.click(anchorTagSelector);
+    const buttonSelector = ".home__row > div:nth-child(1) > button";
+    const anchorTagSelector = ".header__optionBasket";
+    await this.page.click(buttonSelector);
+    await this.page.click(anchorTagSelector);
 
-    // const minusButtonSelector =
-    //   ".checkoutProduct_btn_group > button:nth-child(1)";
-    // await this.page.click(minusButtonSelector);
+    const minusButtonSelector =
+      ".checkoutProduct_btn_group > button:nth-child(1)";
+    await this.page.click(minusButtonSelector);
 
-    // const productElement = await this.page.$(".checkoutProduct");
+    const productElement = await this.page.$(".checkoutProduct");
 
-    // expect(productElement).toBeFalsy();
+    expect(productElement).toBeFalsy();
   });
 });
