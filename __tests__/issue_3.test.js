eceec571ad43f5601451a1d5af7a88585203c82c
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2024-01-11 10:48:38 +0530

    actions testing

--- __tests__/issue_3.test.js
+++ __tests__/issue_3.test.js
@@ -5,18 +5,18 @@ describe("Issue #3", () => {
     await this.page.setViewport({ width: 1280, height: 1024 });
   });
 
-  // it("should not be able to order with negative qty", async () => {
-  //   const buttonSelector = ".home__row > div:nth-child(1) > button";
-  //   const anchorTagSelector = ".header__optionBasket";
-  //   await this.page.click(buttonSelector);
-  //   await this.page.click(anchorTagSelector);
+  it("should not be able to order with negative qty", async () => {
+    // const buttonSelector = ".home__row > div:nth-child(1) > button";
+    // const anchorTagSelector = ".header__optionBasket";
+    // await this.page.click(buttonSelector);
+    // await this.page.click(anchorTagSelector);
 
-  //   const minusButtonSelector =
-  //     ".checkoutProduct_btn_group > button:nth-child(1)";
-  //   await this.page.click(minusButtonSelector);
+    // const minusButtonSelector =
+    //   ".checkoutProduct_btn_group > button:nth-child(1)";
+    // await this.page.click(minusButtonSelector);
 
-  //   const productElement = await this.page.$(".checkoutProduct");
+    // const productElement = await this.page.$(".checkoutProduct");
 
-  //   expect(productElement).toBeFalsy();
-  // });
+    // expect(productElement).toBeFalsy();
+  });
 });
